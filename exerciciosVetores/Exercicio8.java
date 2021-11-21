3e8cc5475f2e102d7c8e12142127ffc3ad2e254e
HugoSalesSilva <93701716+HugoSalesSilva@users.noreply.github.com>
2021-11-21 17:22:12 -0300

    Add files via upload

---
+++ exerciciosVetores/Exercicio8.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Exercicio8 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int a[] ,n, i;
+		
+		final int TAM = 10;
+		
+		a = new int[TAM];
+		
+		System.out.println("Coloque os números:");
+		for(i=0;i<TAM;i++) {
+			a[i]=ler.nextInt();
+		}
+		
+		System.out.println("Coloque o elemento X:");
+		n=ler.nextInt();
+		
+		for(i=0;i<TAM;i++) {
+			if(a[i]==n) {
+				System.out.println("O elemento X de valor "+n+" está em A na "+(i+1)+" posição.");
+			}
+		}
+		
+	}
+
+}
